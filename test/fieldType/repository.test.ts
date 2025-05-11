0f1909b0cfb47e7d2da1f2aa93c478b9a5266e45
Tutuacs <arthursilva.rs1@gmail.com>
2025-05-10 22:53:43 -0300

    :sparkles: TDD: fieldType repository

---
+++ test/fieldType/repository.test.ts
@@ -0,0 +1,102 @@
+import { describe, expect, it } from "bun:test"
+import { FieldType } from "../../src/fieldType/types"
+import { fieldTypeRepository } from "../../src/fieldType/repository"
+
+describe("FieldTypeRepository Repository", () => {
+    const fieldType = new fieldTypeRepository()
+
+    it("should create a fieldType", async () => {
+        const id: number = 1
+        const data: FieldType = {
+            name: "FieldType",
+            type: "STRING",
+            optional: false,
+        }
+
+        const fieldTypeCreated = await fieldType.create(data)
+        const exists = await fieldType.exist(id)
+        if (!exists) {
+            expect(fieldTypeCreated).not.toBe(null)
+            expect(fieldTypeCreated!.name).toBe(data.name)
+            expect(fieldTypeCreated!.type).toBe(data.type)
+            expect(fieldTypeCreated!.optional).toBe(data.optional)
+            return
+        }
+        
+        expect(fieldTypeCreated).not.toBe(null)
+        // expect(fieldTypeCreated).toBe(null)
+    })
+
+    it("should find a fieldType", async () => {
+        const id: number = 1
+        const idBigInt: bigint = BigInt(id)
+        const fieldTypeFound = await fieldType.find(id)
+        expect(fieldTypeFound).not.toBe(null)
+        expect(fieldTypeFound!.id).toBe(idBigInt)
+        expect(fieldTypeFound!.name).toBe("FieldType")
+        expect(fieldTypeFound!.type).toBe("STRING")
+        expect(fieldTypeFound!.optional).toBe(false)
+    })
+
+    it("should list fieldTypes", async () => {
+        const fieldTypes = await fieldType.list({})
+        expect(fieldTypes).not.toBe(null)
+        expect(fieldTypes.length).toBeGreaterThan(0)
+    })
+
+    it("should delete a fieldType", async () => {
+        const id: number = 2
+        const idBigInt: bigint = BigInt(id)
+        const data: FieldType = {
+            id,
+            name: "FieldType deleted",
+            type: "STRING",
+            optional: false,
+        }
+        // Create
+        const created = await fieldType.create(data)
+        expect(created).not.toBe(null)
+        expect(created!.name).toBe(data.name)
+        expect(created!.type).toBe(data.type)
+        expect(created!.optional).toBe(data.optional)
+        // Check if it exists
+        const exists = await fieldType.exist(id)
+        expect(exists).toBe(true)
+        // Delete
+        const deleted = await fieldType.delete(id)
+        expect(deleted).not.toBe(null)
+        expect(deleted!.id).toBe(idBigInt)
+        // Check if it was deleted correctly
+        const existsAfterDelete = await fieldType.exist(id)
+        expect(existsAfterDelete).toBe(false)
+    })
+
+    it("should update a fieldType", async () => {
+        const id: number = 1
+        const idBigInt: bigint = BigInt(id)
+        const data: FieldType = {
+            name: "FieldType updated",
+            type: "NUMBER",
+            optional: true,
+        }
+        // Update
+        const updated = await fieldType.update(id, data)
+        expect(updated).not.toBe(null)
+        expect(updated!.id).toBe(idBigInt)
+        expect(updated!.name).toBe(data.name)
+        expect(updated!.type).toBe(data.type)
+        expect(updated!.optional).toBe(data.optional)
+        // Revert
+        const revert = await fieldType.update(id, {
+            name: "FieldType",
+            type: "STRING",
+            optional: false,
+        })
+        expect(revert).not.toBe(null)
+        expect(revert!.id).toBe(idBigInt)
+        expect(revert!.name).toBe("FieldType")
+        expect(revert!.type).toBe("STRING")
+        expect(revert!.optional).toBe(false)
+    })
+
+})
