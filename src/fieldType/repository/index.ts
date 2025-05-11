6c5388bea16697202e4fcc5dba11d202f7265540
Tutuacs <arthursilva.rs1@gmail.com>
2025-05-10 22:55:08 -0300

    :sparkles: create: repository classes

--- src/fieldType/repository/index.ts
+++ src/fieldType/repository/index.ts
@@ -1,4 +1,5 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "../../prisma/client";
+import { FieldType } from "../types";
 
 export class fieldTypeRepository {
 
@@ -6,6 +7,52 @@ export class fieldTypeRepository {
 
     constructor() {
         this.prisma = new PrismaClient();
+    }
+
+    async exist(id: number) {
+        return 0 < await this.prisma.fieldType.count({
+            where: {
+                id: id,
+            },
+        });
+    }
+
+    async create(data: FieldType) {
+        return await this.prisma.fieldType.create({
+            data,
+        });
+    }
+
+    async delete(id: number) {
+        return await this.prisma.fieldType.delete({
+            where: {
+                id: id,
+            },
+        });
+    }
+
+    async find(id: number) {
+        return await this.prisma.fieldType.findUnique({
+            where: {
+                id: id,
+            },
+        });
+    }
+
+    async list({ skip, take }: {skip?: number , take?: number }) {
+        return await this.prisma.fieldType.findMany({
+            skip,
+            take,
+        });
+    }
+
+    async update(id: number, data: FieldType) {
+        return await this.prisma.fieldType.update({
+            where: {
+                id: id,
+            },
+            data,
+        });
     }
 
 }
