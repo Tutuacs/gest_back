6c3f6ec9af2b1c47bd6650d156c762dc7a8a36a6
Tutuacs <arthursilva.rs1@gmail.com>
2025-05-10 22:25:50 -0300

    :boom: fix: updateBy to object

--- src/common/types.ts
+++ src/common/types.ts
@@ -1,6 +1,10 @@
 import { t } from "elysia";
 
 export const id = t.Optional(t.Number());
-export const updatedAt = t.Optional(t.Date());
-export const updatedBy = t.Optional(t.Number());
 export const createdAt = t.Optional(t.Date());
+export const updatedAt = t.Optional(t.Date());
+export const updatedBy = t.Optional(t.Object({
+    connect: t.Object({
+        id: t.Number(),
+    }),
+}));
