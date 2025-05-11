ad745c0a1a15543b9f9a52eb393a981cb03a5994
Tutuacs <arthursilva.rs1@gmail.com>
2025-05-10 21:19:15 -0300

    :sparkles: TDD: auth repository

---
+++ test/auth/repository.test.ts
@@ -0,0 +1,49 @@
+import { describe, expect, it } from "bun:test";
+import { authRepository } from "../../src/auth/repository";
+
+describe("Auth Repository", () => {
+    const auth = new authRepository()
+
+    it("should return true if user exists", async () => {
+        const user = await auth.existsUser("emailficticio@gmail.com")
+        expect(user).toBe(false)
+    })
+
+    it("should return null with incorrect credentials", async () => {
+        const user = await auth.login("arthur@gmail.com", "123456")
+        expect(user).toBe(null)
+    })
+
+    it("should return null with incorrect credentials", async () => {
+        const user = await auth.login("emailficticio@gmail.com", "123456")
+        expect(user).toBe(null)
+    })
+
+    it("should test the validation and creation of hashed password", async () => {
+        const pass = await auth.hashPassword("123456")
+        expect(pass).toBeString()
+        expect(pass).not.toBe("123456")
+        const valid = await auth.validPassword("123456", pass)
+        expect(valid).toBe(true)
+    });
+
+    it("should return the user", async () => {
+        const credentials = {
+            email: "admin@admin.com",
+            password: "123",
+        }
+
+        const exists = await auth.existsUser(credentials.email)
+        if (!exists) {
+            const user = await auth.register(credentials.email, credentials.password)
+            expect(user).not.toBe(null)
+            expect(user!.password).not.toBe(credentials.password)
+            expect(await auth.validPassword(credentials.password, user!.password)).toBe(true)
+            return
+        }
+
+        const user = await auth.login(credentials.email, credentials.password)
+        expect(user).not.toBe(null)
+    })
+
+})
