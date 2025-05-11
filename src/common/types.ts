11147e9032ccd0ef3ad07d89c909986a8e158ff0
Tutuacs <arthursilva.rs1@gmail.com>
2025-05-10 22:02:19 -0300

    :boom: change: createdAt optional

--- src/common/types.ts
+++ src/common/types.ts
@@ -3,4 +3,4 @@ import { t } from "elysia";
 export const id = t.Optional(t.Number());
 export const updatedAt = t.Optional(t.Date());
 export const updatedBy = t.Optional(t.Number());
-export const createdAt = t.Date();
+export const createdAt = t.Optional(t.Date());
