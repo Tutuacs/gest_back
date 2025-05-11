4f787ced75b734c860e6ffdf3c7e8548ab96ab33
Tutuacs <arthursilva.rs1@gmail.com>
2025-05-10 22:25:16 -0300

    :sparkles: TDD: equipamentType repository

---
+++ test/equipamentType/repository.test.ts
@@ -0,0 +1,95 @@
+import { describe, expect, it } from "bun:test"
+import { equipamentTypeRepository } from "../../src/equipamentType/repository"
+import { EquipamentType } from "../../src/equipamentType/types"
+
+describe("EquipamentTypeRepository Repository", () => {
+    const eqType = new equipamentTypeRepository()
+
+    it("should create an equipamentType", async () => {
+        const data: EquipamentType = {
+            name: "EquipamentType",
+            description: "Description",
+        }
+
+        const exists = await eqType.existName(data.name)
+        const equipamentType = await eqType.create(data)
+
+        if (!exists) {
+            expect(equipamentType).not.toBe(null)
+            expect(equipamentType!.name).toBe(data.name)
+            expect(equipamentType!.description).toBe(data.description)
+            return
+        }
+
+        expect(equipamentType).toBe(null)
+    })
+
+    it("should find an equipamentType", async () => {
+        const id: number = 1
+        const idBigInt: bigint = BigInt(id)
+        const equipamentType = await eqType.find(id)
+        expect(equipamentType).not.toBe(null)
+        expect(equipamentType!.id).toBe(idBigInt)
+        expect(equipamentType!.name).toBe("EquipamentType")
+        expect(equipamentType!.description).toBe("Description")
+    })
+
+    it("should list equipamentTypes", async () => {
+        const equipamentTypes = await eqType.list({ skip: 0, take: 10 })
+        expect(equipamentTypes).not.toBe(null)
+        expect(equipamentTypes.length).toBeGreaterThan(0)
+    })
+
+    it("should delete an equipamentType", async () => {
+        const id: number = 2
+        const idBigInt: bigint = BigInt(id)
+        const data: EquipamentType = {
+            id: id,
+            name: "EquipamentType deleted",
+            description: "Description",
+        }
+        // Create
+        const created = await eqType.create(data)
+        expect(created).not.toBe(null)
+        expect(created!.name).toBe(data.name)
+        expect(created!.description).toBe(data.description)
+        // Check if it exists
+        const exists = await eqType.existName(data.name)
+        expect(exists).toBe(true)
+        // Check if it was created correctly
+        const equipamentType = await eqType.find(id)
+        expect(equipamentType).not.toBe(null)
+        expect(equipamentType!.id).toBe(idBigInt)
+        expect(equipamentType!.name).toBe(data.name)
+        expect(equipamentType!.description).toBe(data.description)
+        // Delete
+        const deleted = await eqType.delete(id)
+        expect(deleted).not.toBe(null)
+        expect(deleted!.id).toBe(idBigInt)
+        const existsAfterDelete = await eqType.existName(data.name)
+        expect(existsAfterDelete).toBe(false)
+        const equipamentTypeAfterDelete = await eqType.find(id)
+        expect(equipamentTypeAfterDelete).toBe(null)
+    })
+
+    it("should update an equipamentType", async () => {
+        const id: number = 1
+        const idBigInt: bigint = BigInt(id)
+        const data: EquipamentType = {
+            name: "EquipamentType Updated",
+            description: "Description Updated",
+        }
+
+        const equipamentType = await eqType.update(id, data)
+        expect(equipamentType).not.toBe(null)
+        expect(equipamentType!.id).toBe(idBigInt)
+        expect(equipamentType!.name).toBe(data.name)
+        expect(equipamentType!.description).toBe(data.description)
+        const revert = {
+            name: "EquipamentType",
+            description: "Description",
+        }
+        await eqType.update(id, revert)
+    })
+
+})
