31824ceb669cf33e5d4a92b558d60ecc01df9316
Tutuacs <arthursilva.rs1@gmail.com>
2025-05-12 15:38:59 -0300

    :sparkles: add: routes on fieldType

--- src/fieldType/index.ts
+++ src/fieldType/index.ts
@@ -1,3 +1,13 @@
 import Elysia from "elysia";
+import { create } from "./controllers/create";
+import { delet } from "./controllers/delete";
+import { find } from "./controllers/find";
+import { list } from "./controllers/list";
+import { update } from "./controllers/update";
 
-export const FieldTypeHandler = new Elysia({name: "FieldTypeHandler", prefix: "/field-type", tags: ["Field Type"]})
+export const FieldTypeHandler = new Elysia({ name: "FieldTypeHandler", prefix: "/field-type", tags: ["Field Type"] })
+    .use(create)
+    .use(delet)
+    .use(find)
+    .use(list)
+    .use(update)
