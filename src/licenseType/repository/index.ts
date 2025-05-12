5bbcfa2c8c63eaa233bd4eaffd24f93ecc1cb514
Tutuacs <arthursilva.rs1@gmail.com>
2025-05-11 22:05:26 -0300

    :sparkles: TDD: new repositorys

--- src/licenseType/repository/index.ts
+++ src/licenseType/repository/index.ts
@@ -1,10 +1,51 @@
 import { PrismaClient } from "../../prisma/client";
+import { LicenseType } from "../types";
 
 export class licenseTypeRepository {
     private prisma: PrismaClient;
 
     constructor() {
         this.prisma = new PrismaClient();
+    }
+
+    async exist(id: number) {
+        return 0 < await this.prisma.licenseType.count({
+            where: {
+                id: id,
+            },
+        });
+    }
+
+    async create(data: LicenseType) {
+        return this.prisma.licenseType.create({
+            data,
+        });
+    }
+
+    async delete(id: number) {
+        return this.prisma.licenseType.delete({
+            where: { id },
+        });
+    }
+
+    async find(id: number) {
+        return this.prisma.licenseType.findUnique({
+            where: { id },
+        });
+    }
+
+    async list({skip, take}: {skip?: number, take?: number}) {
+        return this.prisma.licenseType.findMany({
+            skip,
+            take,
+        });
+    }
+
+    async update(id: number, data: Partial<LicenseType>) {
+        return this.prisma.licenseType.update({
+            where: { id },
+            data,
+        });
     }
 
 }
