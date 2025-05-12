284ed6ae106f83acf7c3ee9cdd08d349a4ed7eb4
Tutuacs <arthursilva.rs1@gmail.com>
2025-05-11 22:03:10 -0300

    :sparkles: TDD: new repositorys

---
+++ test/licenseType/repository.test.ts
@@ -0,0 +1,105 @@
+import { describe, expect, it } from "bun:test"
+import { LicenseType } from "../../src/licenseType/types"
+import { licenseTypeRepository } from "../../src/licenseType/repository"
+
+describe("LicenseTypeRepository Repository", () => {
+    const licenseType = new licenseTypeRepository()
+
+    it("should create a licenseType", async () => {
+        const id: number = 1
+        const data: LicenseType = {
+            name: "LicenseType",
+            description: "LicenseType description",
+            type: "STRING",
+            unvalidOnMoviment: false,
+        }
+
+        const licenseTypeCreated = await licenseType.create(data)
+        const exists = await licenseType.exist(id)
+
+        if (!exists) {
+            expect(licenseTypeCreated).not.toBe(null)
+            expect(licenseTypeCreated!.name).toBe(data.name)
+            expect(licenseTypeCreated!.description).toBe(data.description)
+            expect(licenseTypeCreated!.type).toBe(data.type)
+            expect(licenseTypeCreated!.unvalidOnMoviment).toBe(data.unvalidOnMoviment)
+            return
+        }
+        
+        expect(licenseTypeCreated).not.toBe(null)
+        // expect(licenseTypeCreated).toBe(null)
+    })
+
+    it("should find a licenseType", async () => {
+        const id: number = 1
+        const licenseTypeFound = await licenseType.find(id)
+        expect(licenseTypeFound).not.toBe(null)
+        expect(licenseTypeFound!.id).toBe(id)
+        expect(licenseTypeFound!.name).toBe("LicenseType")
+        expect(licenseTypeFound!.description).toBe("LicenseType description")
+        expect(licenseTypeFound!.type).toBe("STRING")
+        expect(licenseTypeFound!.unvalidOnMoviment).toBe(false)
+    })
+
+    it("should list licenseTypes", async () => {
+        const licenseTypes = await licenseType.list({})
+        expect(licenseTypes).not.toBe(null)
+        expect(licenseTypes.length).toBeGreaterThan(0)
+    })
+
+    it("should delete a licenseType", async () => {
+        const id: number = 2
+        const data: LicenseType = {
+            id,
+            name: "LicenseType deleted",
+            description: "LicenseType deleted description",
+            type: "STRING",
+            unvalidOnMoviment: false,
+        }
+        // Create
+        const created = await licenseType.create(data)
+        expect(created).not.toBe(null)
+        expect(created!.name).toBe(data.name)
+        expect(created!.description).toBe(data.description)
+        expect(created!.type).toBe(data.type)
+        expect(created!.unvalidOnMoviment).toBe(data.unvalidOnMoviment)
+
+        // Delete
+        const deleted = await licenseType.delete(id)
+        expect(deleted).not.toBe(null)
+        expect(deleted.id).toBe(id)
+    })
+
+    it("should update a licenseType", async () => {
+        const id: number = 1
+        const data: LicenseType = {
+            id,
+            name: "LicenseType updated",
+            description: "LicenseType updated description",
+            type: "STRING",
+            unvalidOnMoviment: false,
+        }
+        // Update
+        const updated = await licenseType.update(id, data)
+        expect(updated).not.toBe(null)
+        expect(updated!.name).toBe(data.name)
+        expect(updated!.description).toBe(data.description)
+        expect(updated!.type).toBe(data.type)
+        expect(updated!.unvalidOnMoviment).toBe(data.unvalidOnMoviment)
+        // Check if it was updated correctly
+        const updatedCheck = await licenseType.find(id)
+        expect(updatedCheck).not.toBe(null)
+        expect(updatedCheck!.name).toBe(data.name)
+        expect(updatedCheck!.description).toBe(data.description)
+        expect(updatedCheck!.type).toBe(data.type)
+        expect(updatedCheck!.unvalidOnMoviment).toBe(data.unvalidOnMoviment)
+        // Revert
+        await licenseType.update(id, {
+            name: "LicenseType",
+            description: "LicenseType description",
+            type: "STRING",
+            unvalidOnMoviment: false,
+        })
+    })
+
+})
---
+++ test/movimentation/repository.test.ts
@@ -0,0 +1,109 @@
+import { describe, expect, it } from "bun:test"
+import { Movimentation } from "../../src/movimentation/types"
+import { movimentationRepository } from "../../src/movimentation/repository"
+
+describe("MovimentationRepository Repository", () => {
+    const movimentation = new movimentationRepository()
+
+    it("should create a movimentation", async () => {
+        const id: number = 1
+        const data: Movimentation = {
+            id,
+            equipamentId: 1,
+            date: new Date(),
+            description: "Movimentation description",
+            createdAt: new Date(),
+            updatedById: 1,
+        }
+
+        const movimentationCreated = await movimentation.create(data)
+        const exists = await movimentation.exist(id)
+
+        if (!exists) {
+            expect(movimentationCreated).not.toBe(null)
+            expect(movimentationCreated!.equipamentId).toBe(data.equipamentId)
+            expect(movimentationCreated!.date).toBe(data.date)
+            expect(movimentationCreated!.description).toBe(data.description)
+            return
+        }
+        
+        expect(movimentationCreated).not.toBe(null)
+        // expect(movimentationCreated).toBe(null)
+    })
+
+    it("should find a movimentation", async () => {
+        const id: number = 1
+        const movimentationFound = await movimentation.find(id)
+        expect(movimentationFound).not.toBe(null)
+        expect(movimentationFound!.id).toBe(id)
+        expect(movimentationFound!.equipamentId).toBe(1)
+        expect(movimentationFound!.date).toBeInstanceOf(Date)
+        expect(movimentationFound!.description).toBe("Movimentation description")
+    })
+
+    it("should list movimentations", async () => {
+        const movimentations = await movimentation.list({})
+        expect(movimentations).not.toBe(null)
+        expect(movimentations.length).toBeGreaterThan(0)
+    })
+
+    it("should delete a movimentation", async () => {
+        const id: number = 2
+        const data: Movimentation = {
+            id,
+            equipamentId: 1,
+            date: new Date(),
+            description: "Movimentation deleted",
+            createdAt: new Date(),
+            updatedById: 1,
+        }
+        // Create
+        const created = await movimentation.create(data)
+        expect(created).not.toBe(null)
+        expect(created!.equipamentId).toBe(data.equipamentId)
+        expect(created!.date).toBe(data.date)
+        expect(created!.description).toBe(data.description)
+        // Check if it exists
+        const exists = await movimentation.exist(id)
+        expect(exists).toBe(true)
+        // Check if it was created correctly
+        const movimentationFound = await movimentation.find(id)
+        expect(movimentationFound).not.toBe(null)
+        expect(movimentationFound!.id).toBe(id)
+    })
+
+    it("should update a movimentation", async () => {
+        const id: number = 1
+        const data: Movimentation = {
+            id,
+            equipamentId: 1,
+            date: new Date(),
+            description: "Movimentation updated",
+            createdAt: new Date(),
+            updatedById: 1,
+        }
+        // Update
+        const updated = await movimentation.update(id, data)
+        expect(updated).not.toBe(null)
+        expect(updated!.id).toBe(id)
+        expect(updated!.equipamentId).toBe(data.equipamentId)
+        expect(updated!.date).toBe(data.date)
+        expect(updated!.description).toBe(data.description)
+        // Check if it was updated correctly
+        const movimentationFound = await movimentation.find(id)
+        expect(movimentationFound).not.toBe(null)
+        expect(movimentationFound!.id).toBe(id)
+        expect(movimentationFound!.equipamentId).toBe(data.equipamentId)
+        expect(movimentationFound!.date).toBe(data.date)
+        expect(movimentationFound!.description).toBe(data.description)
+        // Revert
+        await movimentation.update(id, {
+            equipamentId: 1,
+            date: new Date(),
+            description: "Movimentation description",
+            createdAt: new Date(),
+            updatedById: 1,
+        })
+    })
+
+})
