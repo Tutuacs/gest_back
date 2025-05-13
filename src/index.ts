d56fcba2a2cde1ddb840eed7742244cad45c52c8
Tutuacs <arthursilva.rs1@gmail.com>
2025-05-13 15:58:33 -0300

    :sparkles: DEBUG: add debug routes /me && /logout

--- src/index.ts
+++ src/index.ts
@@ -14,10 +14,12 @@ import { UserHandler } from "./user";
 import swagger from "@elysiajs/swagger";
 import cors from "@elysiajs/cors";
 import { AuthHandler } from "./auth";
+import { authenticator } from "./jwt";
 
-const app = new Elysia()
+const app = new Elysia({ name: "Main" })
   .use(cors())
   .use(swagger())
+  .use(authenticator)
   .use(AuthHandler)
   .use(EquipamentHandler)
   .use(EquipamentTypeHandler)
@@ -31,6 +33,21 @@ const app = new Elysia()
   .use(ReportFieldValueHandler)
   .use(UpdaterHandler)
   .use(UserHandler)
+  .get("/me", async ({ getInfo, cookie: { auth, userInfo } }) => {
+    const info = await getInfo();
+
+    return {
+      user: info?.user,
+      userInfo: JSON.parse(userInfo.value ?? "{}"),
+    }
+  })
+  .get("/logout", async({ logOut }) => {
+    await logOut();
+
+    return {
+      message: "Logged out",
+    }
+  })
 
 
 app.listen(3000);
