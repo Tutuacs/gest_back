97571d2685d0398b018d130594f9c5d98bdd5e58
Tutuacs <arthursilva.rs1@gmail.com>
2025-05-14 16:15:44 -0300

    :sparkles: BDD: init files structure

---
+++ test/fieldType/routes.spec.ts
@@ -0,0 +1,29 @@
+import { describe, expect, it } from "bun:test";
+
+describe("FieldType Routes", () => {
+
+    describe("Create", () => {
+        it("should", () => {
+            
+        })
+    })
+
+    describe("Find", () => {
+        it("should", () => {
+
+        })
+    })
+
+    describe("List", () => {
+        it("should", () => {
+
+        })
+    })
+
+    describe("Update", () => {
+        it("should", () => {
+
+        })
+    })
+
+})
---
+++ test/reportFieldType/routes.spec.ts
@@ -0,0 +1,29 @@
+import { describe, expect, it } from "bun:test";
+
+describe("ReportFieldType Routes", () => {
+
+    describe("Create", () => {
+        it("should", () => {
+            
+        })
+    })
+
+    describe("Find", () => {
+        it("should", () => {
+
+        })
+    })
+
+    describe("List", () => {
+        it("should", () => {
+
+        })
+    })
+
+    describe("Update", () => {
+        it("should", () => {
+
+        })
+    })
+
+})
