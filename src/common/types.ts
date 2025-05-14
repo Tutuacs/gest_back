e51030ee7d7b8e1af96b16da7c2944af9279fad5
Tutuacs <arthursilva.rs1@gmail.com>
2025-05-14 09:33:02 -0300

    :boom: update: field updatedBy type

--- src/common/types.ts
+++ src/common/types.ts
@@ -3,4 +3,4 @@ import { t } from "elysia";
 export const id = t.Optional(t.Number());
 export const createdAt = t.Optional(t.Date());
 export const updatedAt = t.Optional(t.Date());
-export const updatedById = t.Optional(t.Number());
+export const updatedById = t.Optional(t.Union([t.Number(), t.Null()]));
