d61e83738e59112fb6bf49ecf92496e52551e1b2
Tutuacs <arthursilva.rs1@gmail.com>
2025-05-14 09:40:00 -0300

    :sparkles: BDD: init update route

--- src/equipamentType/controllers/update.ts
+++ src/equipamentType/controllers/update.ts
@@ -2,13 +2,51 @@ import Elysia from "elysia";
 import { validationSchema } from "./validation/update";
 import { authenticator } from "../../jwt";
 import { equipamentTypeRepository } from "../repository";
+import { ROLE } from "../../prisma";
 
 export const update = new Elysia({ name: "UpdateEquipamentType" })
     .use(authenticator)
     .decorate("equipamentTypeRepository", new equipamentTypeRepository())
-    .put("/:id", ({ body, params, equipamentTypeRepository, getInfo, set }) => {
+    .put("/:id", async({ body, params, equipamentTypeRepository, getInfo, set }) => {
+
+        const token = await getInfo();
+
+        if (!token) {
+            set.status = 401;
+            return {
+                message: "",
+            };
+        }
+
+        if (token.user.role != ROLE.MASTER) {
+            set.status = 405;
+            return {
+                message: "",
+            }
+        }
+
+        const exist = await equipamentTypeRepository.exist(params.id);
+        if (!exist) {
+            set.status = 404;
+            return {
+                message: "Equipament type not found" as string,
+            };
+        }
+
+        const canUpdate = await equipamentTypeRepository.canUpdate(params.id, body.equipamentType.name);
+
+        if (!canUpdate) {
+            set.status = 409;
+            return {
+                message: "Equipament type already exists" as string,
+            };
+        }
+
+        body.equipamentType.updatedById = token?.user.id;
+
+        const updated = await equipamentTypeRepository.update(params.id, body.equipamentType);
+
         return {
-            message: `Equipament type with id ${params.id} updated successfully`,
-            data: body,
+            equipamentType: updated,
         };
     }, validationSchema);
